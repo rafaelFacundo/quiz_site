62de0c78bf18881ff32e9a8d577e0a946b814b64
RafaelFacundo <rafaelfacundo63@gmail.com>
2023-02-06 21:49:22 -0300

    playQuiz style done, doing logic

--- src/pages/PlayQuizPage/components/QuestionAndOptions/index.jsx
+++ src/pages/PlayQuizPage/components/QuestionAndOptions/index.jsx
@@ -8,6 +8,7 @@ const MainContent = styled.div`
     width: 100%;
     height: 100%;
     flex-direction: column;
+    position: relative;
 `
 const AnswerOption = styled.button`
     width: 350px;
@@ -21,14 +22,44 @@ const AnswerOption = styled.button`
         background-color: #696969;
     }
 `;
-const OptionsDiv = styled.div``;
+const OptionsDiv = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+`;
 const QuizQuestion = styled.h2`
     color: black;
+`;
+const AnswersRecordDiv = styled.div`
+    position: absolute;
+    top: 0px;
+    left: 0px;
+`;
+const AnswersRecordContentDiv = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+`;
+const AnswerRecord = styled.div`
+    background-color: ${props => props.answerColor || "gray"};
+    width: 100px;
+    height: 10px;
+    margin-right: 5px;
 `;
 
 export default function QuestionAndOptions() {
     return(
         <MainContent>
+            <AnswersRecordDiv>
+                <AnswersRecordContentDiv>
+                    <AnswerRecord answerColor={"red"} />
+                    <AnswerRecord answerColor={"green"} />
+                    <AnswerRecord answerColor={"yellow"} />
+                    <AnswerRecord />
+                </AnswersRecordContentDiv>
+            </AnswersRecordDiv>
             <QuizQuestion>Olá mundo</QuizQuestion>
             <OptionsDiv>
                 <AnswerOption>opção 1</AnswerOption>
